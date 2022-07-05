d7de00d687a3fac8216bdf6dc591f7591193fb89
YitongXX <100065081+YitongXX@users.noreply.github.com>
2022-07-05 17:28:00 +0800

    Update API url

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
-
-document.getElementById("btn").addEventListener("click",()=>{
-    fetch("https://apis.scrimba.com/bored/api/activity")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        document.getElementById("text-container").textContent = data.activity;
-    });
-})
+document.getElementById("btn").addEventListener("click", () => {
+    fetch("https://www.boredapi.com/api/activity")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            document.getElementById("text-container").textContent =
+                data.activity;
+        });
+});
